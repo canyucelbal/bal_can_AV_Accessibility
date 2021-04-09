a43a75013b8456ebaf2962121051eda9a1598ba2
Can Yucel Bal <canyucelbal@gmail.com>
2021-04-09 18:03:06 -0400

    full screen adjusted. folder structure set

--- js/components/MiniFile.js
+++ js/components/MiniFile.js
@@ -13,7 +13,7 @@ export default {
   template: `
     <router-link :to="'/detail/'+id">
       <div class="mini-file">
-        <img :src="thumbnail" :alt="name" class="img-fluid">
+        <img :src="'images/' + file.thumbnail" :alt="name" class="img-fluid">
         <p>{{ name }}</p>
       </div>
     </router-link>
